d51a371a000567d5dc33d7366c668cb0c26d901d
Thomas-Ishida <ishida.thomas@hotmail.com>
2025-09-04 12:19:45 -0400

    Added messsage display on button

--- app/src/main/java/edu/temple/helloapp/MainActivity.kt
+++ app/src/main/java/edu/temple/helloapp/MainActivity.kt
@@ -1,14 +1,21 @@
 package edu.temple.helloapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.Chip
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Chip>(R.id.chip).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text = "You clicked the button!"
+        }
     }
 }
